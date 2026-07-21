b488753a6582129ea2ab2318cd07902d7353621d
Chan-b0b <2015170731@korea.ac.kr>
2026-07-21 14:22:58 +0900

    merge

--- LGES/vla_training/upload_weights.py
+++ LGES/vla_training/upload_weights.py
@@ -25,6 +25,8 @@ META = {
     "smolvla_film_0708_abs_prefix_mask1": ("Chanho-Lee/lges_case_pick_0708_abs", "FiLM v2 from base, cond=contact,fz,seal inject=prefix mask_force=1, 30k"),
     "smolvla_film_0708_dF_prefix_mask1":     ("Chanho-Lee/lges_case_pick_0708_dF",     "FiLM v2 from base, cond=contact,fz,seal,dfmag inject=prefix mask_force=1 dfmag_tau=5, 30k, state 16"),
     "smolvla_film_0708_abs_dF_prefix_mask1": ("Chanho-Lee/lges_case_pick_0708_abs_dF", "FiLM v2 from base, cond=contact,fz,seal,dfmag inject=prefix mask_force=1 dfmag_tau=5, 30k, state 16"),
+    "smolvla_film_0708_dF_prefix_mask1_os10":     ("Chanho-Lee/lges_case_pick_0708_dF",     "FiLM v2 from base, cond=contact,fz,seal,dfmag inject=prefix mask_force=1, contact-transition oversampling x10, 30k, state 16"),
+    "smolvla_film_0708_abs_dF_prefix_mask1_os10": ("Chanho-Lee/lges_case_pick_0708_abs_dF", "FiLM v2 from base, cond=contact,fz,seal,dfmag inject=prefix mask_force=1, contact-transition oversampling x10, 30k, state 16"),
 }
 
 api = HfApi()
